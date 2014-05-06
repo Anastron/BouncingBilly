b4219679928a56e46c4bdda9b61a81a34709c80a
Armin <armin_benz@web.de>
2014-05-06 15:38:42 +0200

    recreate Project with gradle

---
+++ android/src/sam/softwaredeveloping/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package sam.softwaredeveloping.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import sam.softwaredeveloping.BouncingBilly;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BouncingBilly(), config);
+	}
+}
---
+++ desktop/src/sam/softwaredeveloping/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package sam.softwaredeveloping.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import sam.softwaredeveloping.BouncingBilly;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BouncingBilly(), config);
+	}
+}
