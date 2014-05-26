62044cc7ea39a72a11d28d7584e3d297c53d8cf6
Armin <armin_benz@web.de>
2014-05-26 21:15:17 +0200

    added first boxes

---
+++ core/src/sam/bouncingbilly/handlers/B2DVars.java
@@ -0,0 +1,6 @@
+package sam.bouncingbilly.handlers;
+
+public class B2DVars {
+
+	public static final float PPM = 100;
+}
--- core/src/sam/bouncingbilly/states/Play.java
+++ core/src/sam/bouncingbilly/states/Play.java
@@ -1,14 +1,62 @@
 package sam.bouncingbilly.states;
 
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-
+import static sam.bouncingbilly.handlers.B2DVars.PPM;
 import sam.bouncingbilly.handlers.GameStateManager;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 public class Play extends GameState{
 
-	private BitmapFont font = new BitmapFont();
+	private World world;
+	private Box2DDebugRenderer b2dr;
+	
+	private OrthographicCamera b2dCam;
 	
 	public Play(GameStateManager gsm){
 		super(gsm);
+		
+		world = new World(new Vector2(0, -9.81f), true);
+		b2dr = new Box2DDebugRenderer();
+		
+		// create platform
+		BodyDef bdef = new BodyDef();
+		bdef.position.set(160 / PPM , 120 / PPM);
+		// static body - dont move, unaffected by forces
+		// kinematic body - dont get affected forces 
+		// dynamic body - alwayy get affected by forces
+		bdef.type = BodyType.StaticBody;
+		Body body = world.createBody(bdef);
+		
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(50 / PPM, 5 / PPM);		
+		FixtureDef fdef = new FixtureDef();
+		fdef.shape = shape;
+		body.createFixture(fdef);
+		
+		// create falling box
+		bdef.position.set(160 / PPM, 200 / PPM);
+		bdef.type = BodyType.DynamicBody;
+		body = world.createBody(bdef);
+		
+		
+		shape.setAsBox(5 / PPM, 5 / PPM);
+		fdef.shape = shape;
+		fdef.restitution = 0.8f;
+		body.createFixture(fdef);
+		
+		// set up box2d cam
+		b2dCam = new OrthographicCamera();
+		b2dCam.setToOrtho(false, 1280 / PPM , 720 / PPM);
 	}
 
 	@Override
@@ -19,16 +67,18 @@ public class Play extends GameState{
 
 	@Override
 	public void update(float dt) {
-		// TODO Auto-generated method stub
+		world.step(dt, 6, 2);
 		
 	}
 
 	@Override
 	public void render() {
-		sb.setProjectionMatrix(cam.combined);
-		sb.begin();
-		font.draw(sb, "play state", 100, 100);
-		sb.end();
+		// clear screen
+		Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		// draw box2d world
+		b2dr.render(world, b2dCam.combined);
+		
 	}
 
 	@Override
