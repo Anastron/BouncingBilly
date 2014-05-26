597de5fcd7fe573682bc7db1b7e321eaf4f9a029
Armin <armin_benz@web.de>
2014-05-26 19:47:51 +0200

    changed mutch things :)

---
+++ core/src/sam/bouncingbilly/states/Play.java
@@ -0,0 +1,40 @@
+package sam.bouncingbilly.states;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
+import sam.bouncingbilly.handlers.GameStateManager;
+public class Play extends GameState{
+
+	private BitmapFont font = new BitmapFont();
+	
+	public Play(GameStateManager gsm){
+		super(gsm);
+	}
+
+	@Override
+	public void handleInput() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(float dt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		sb.setProjectionMatrix(cam.combined);
+		sb.begin();
+		font.draw(sb, "play state", 100, 100);
+		sb.end();
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- core/src/sam/softwaredeveloping/BouncingBilly.java
+++ core/src/sam/softwaredeveloping/BouncingBilly.java
@@ -1,23 +1,72 @@
 package sam.softwaredeveloping;
 
-import sam.softwaredeveloping.screens.MainMenuScreen;
+import sam.bouncingbilly.handlers.GameStateManager;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class BouncingBilly extends Game {
+public class BouncingBilly implements ApplicationListener {
 	
 	public static final String TITLE = "Bouncing Billy", VERSION = "0.0.0";
+
+	public static final float STEP = 1 / 60f;
+	private float accum;
 	
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch sb;
+	private OrthographicCamera cam;
+	private OrthographicCamera hudCam;
+	
+	private GameStateManager gsm;
 	
 	@Override
-	public void create () {
-
-		((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenuScreen());
+	public void create() {
+		sb = new SpriteBatch();
+		cam = new OrthographicCamera();
+		cam.setToOrtho(false, 1280, 720);
+		hudCam = new OrthographicCamera();
+		hudCam.setToOrtho(false, 1280, 720);
+		
+		gsm = new GameStateManager(this);
 	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		accum += Gdx.graphics.getDeltaTime();
+		while(accum >= STEP){
+			accum -= STEP;
+			gsm.update(STEP);
+			gsm.render();
+		}
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public SpriteBatch getSpriteBatch() {return sb;}
+	public OrthographicCamera getCamera() {return cam;}
+	public OrthographicCamera getHUDCamera() {return hudCam;}
 
 }
